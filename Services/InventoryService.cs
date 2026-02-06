bbefdf254ddc3add3b132d5c375305bd032966c5
Wandise Ndisoh <wandise1589@icloud.com>
2026-02-06 20:52:53 +0200

    Add project files.

---
+++ Services/InventoryService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RetailFlowManager.Services
+{
+    public class InventoryService
+    {
+        public bool IsStockAvailable(int currentStock, int requestedAmount)
+        {
+            if (requestedAmount <= 0) return false;
+            return currentStock >= requestedAmount;
+        }
+    }
+}
